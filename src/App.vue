908a79387eb30b92b71563d18b154c301980bf1b
D3stros <patrick.fallmann@hotmail.com>
2022-08-27 21:20:49 +0200

    Adding composition api

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,37 @@
 <template>
-<div class="app">hello, hyrule</div>
+<div class="app">
+<p>{{ jobs[0].location }}</p>
+</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, reactive, ref, toRefs } from 'vue';
+import Job from './types/Job'
 
 export default defineComponent({
   name: 'App',
-  components: {
+  components: {},
+  setup() {
+    // const state = reactive({
+    //   name: 'Link',
+    //   age: 25 as string | number
+    // })
 
+    // state.name = 999
+    // state.age = 26
+    // return {...toRefs(state)}
+
+    // const name = ref('Link')
+    // const age = ref<number | string>(25)
+
+    // return {name, age}
+
+    const jobs = ref<Job[]>([
+      {title: 'farm worker', location: 'ranch', salary: 30000, id: '1'},
+      {title: 'fisherman', location: 'lake', salary: 21000, id: '2'},
+      {title: 'prison guard', location: 'arizona', salary: 32000, id: '3'}
+    ])
+    return {jobs}
   }
 });
 </script>
